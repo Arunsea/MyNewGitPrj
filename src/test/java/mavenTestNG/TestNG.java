4b917048f1c6f0dffd115b89d630745d7c4ab652
Arunkumar S E <arunkumarse1984@gmail.com>
2019-08-20 20:18:44 +0530

    @Test annotation is implemented

--- src/test/java/mavenTestNG/TestNG.java
+++ src/test/java/mavenTestNG/TestNG.java
@@ -1,5 +1,38 @@
 package mavenTestNG;
 
+import org.testng.annotations.Test;
+
 public class TestNG {
 
+	@Test
+
+	public void openbrowser() {
+		System.out.println("Open browser");
+	}
+
+	@Test
+
+	public void enterURL() {
+		System.out.println("Enter the URL");
+	}
+
+	@Test
+
+	public void searchProduct() {
+		System.out.println("search the product");
+	}
+	
+	@Test
+
+	public void verifyProduct() {
+		System.out.println("verify the product");
+	}
+	
+	@Test
+
+	public void closeBrowser() {
+		System.out.println("close the browser");
+	}
+	
+	
 }
